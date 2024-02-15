b29f16fdf21ad837ef1a8aa68d1e5a149cffc323
Bohdan Dhzus <b.dhzus@yahoo.com>
2024-02-15 14:53:03 +0200

    Fix CMake file & build the lib

---
+++ Framework/build/CMakeFiles/FrameworkRelease.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  ".0.0.1"
+  "CMakeFiles/FrameworkRelease.dir/src/launch.cpp.o"
+  "CMakeFiles/FrameworkRelease.dir/src/launch.cpp.o.d"
+  "libFrameworkRelease.0.0.1.dylib"
+  "libFrameworkRelease.dylib"
+  "libFrameworkRelease.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FrameworkRelease.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
